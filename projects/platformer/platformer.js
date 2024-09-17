40f41ba0613d0ae36a813ae2c370280020c1291c
paco-1 <francisco.vazquez@jpschools.org>
2024-09-17 20:27:16 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -52,7 +52,7 @@ $(function () {
     // example usage: createCollectable(type, x, y, gravity, bounce)
     createCollectable("steve", 500, 300, 20, 0.5); // creates a "steve" collectible at the coordinates (500, 300), falling with a high gravity of 20, and bouncing with 50% bounce
     createCollectable("grace", 500, 300); // creates a "grace" collectible at the coordinates (500, 300), falling with default gravity and bouncing with default bounce %
-
+    createCollectable("max", 800, 300); // creates a "grace" collectible at the coordinates (500, 300), falling with default gravity and bouncing with default bounce %
 
 
     
@@ -61,9 +61,11 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon("left", 600, 1000); // cannon on left wall, 600px down, shooting once per second
-
-
+    createCannon("left", 500, 1000); // cannon on left wall, 600px down, shooting once per second
+    createCannon("right", 400, 1000);
+    createCannon("left", 100, 2000); 
+    createCannon("top", 240, 2000); 
+    createCannon("top", 750, 1000);
 
     
     /////////////////////////////////////////////////
